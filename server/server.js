cab3b3ecf009f70d62e069793527cd4e3c9f6172
R4j4t-Singh <202011061@diu.iiitvadodara.ac.in>
2025-02-15 16:13:55 +0530

    Added dotenv package

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import express from "express";
 import http from "http";
 import { PrismaClient } from "@prisma/client";
 import cors from "cors";
+import dotenv from "dotenv"
+dotenv.config()
 
 const PORT = process.env.PORT || "3000";
 const app = express();
